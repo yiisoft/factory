b1f37c290df454b95d4814ef823825db62d3a0b4
Alexander Makarov <sam@rmcreative.ru>
2020-01-31 00:56:56 +0300

    Add PhpStorm meta for better code completion in IDE

---
+++ .phpstorm.meta.php
@@ -0,0 +1,19 @@
+<?php
+// .phpstorm.meta.php
+
+namespace PHPSTORM_META {
+
+    override(
+        \Yiisoft\Factory\Factory::create(0),
+        map([
+            '' => '@',
+        ])
+    );
+
+    override(
+        \Yiisoft\Factory\Factory::get(0),
+        map([
+            '' => '@',
+        ])
+    );
+}
