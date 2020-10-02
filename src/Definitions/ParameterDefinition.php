9dff6cd0e8142dd1337f5976ae12c7703af33c37
Andrii Vasyliev <sol@hiqdev.com>
2020-10-02 18:19:22 +0300

    Fix constructor parameters injecting (#46)
    * Fix constructor parameters injecting
    
    - constructor with default values is called properly
    - pass all arguments if parameter is variadic
    
    Fixes:
    
    - https://github.com/yiisoft/di/issues/157
    - https://github.com/yiisoft/di/pull/172
    - https://github.com/yiisoft/di/pull/173
    
    Co-authored-by: Alexander Makarov <sam@rmcreative.ru>

---
+++ src/Definitions/ParameterDefinition.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Factory\Definitions;
+
+use ReflectionParameter;
+
+class ParameterDefinition extends ValueDefinition
+{
+    private ReflectionParameter $parameter;
+
+    public function __construct(ReflectionParameter $parameter, $value, string $type = null)
+    {
+        $this->parameter = $parameter;
+        parent::__construct($value, $type);
+    }
+
+    public function getParameter(): ReflectionParameter
+    {
+        return $this->parameter;
+    }
+}
