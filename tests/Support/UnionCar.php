58ead673936270dfe84f91b73f05fb68700f0620
Sergei Predvoditelev <sergey.predvoditelev@gmail.com>
2021-03-30 11:17:55 +0300

    Cleanup test object UnionCar.

--- tests/Support/UnionCar.php
+++ tests/Support/UnionCar.php
@@ -4,11 +4,8 @@ declare(strict_types=1);
 
 namespace Yiisoft\Factory\Tests\Support;
 
-use Yiisoft\Di\Tests\Support\ColorInterface;
-
 class UnionCar
 {
-    public ColorInterface $color;
     private NonExistingEngine|EngineMarkOne|EngineMarkTwo $engine;
 
     public function __construct(NonExistingEngine|EngineMarkOne|EngineMarkTwo $engine)
