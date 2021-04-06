a87388f1b850bbb8e9405720992f0254d291511e
Alexander Makarov <sam@rmcreative.ru>
2021-04-07 00:37:39 +0300

    Improve NotFoundException message and arguments

--- src/Exceptions/NotFoundException.php
+++ src/Exceptions/NotFoundException.php
@@ -6,10 +6,23 @@ namespace Yiisoft\Factory\Exceptions;
 
 use Exception;
 use Psr\Container\NotFoundExceptionInterface;
+use Throwable;
 
 /**
- * NotFoundException is thrown when no entry was found in the container.
+ * NotFoundException is thrown when no definition or class was found in the container for a given ID.
  */
 class NotFoundException extends Exception implements NotFoundExceptionInterface
 {
+    private string $id;
+
+    public function __construct(string $id)
+    {
+        $this->id = $id;
+        parent::__construct("No definition or class found for \"$id\".");
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
 }
