5b997ea98b97119bcad3227c027bf6172c7ce514
Sergei Predvoditelev <sergey.predvoditelev@gmail.com>
2021-07-14 22:07:48 +0300

    More tests for ValueDefinition

--- tests/Unit/Definition/ValueDefinitionTest.php
+++ tests/Unit/Definition/ValueDefinitionTest.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace Yiisoft\Factory\Tests\Unit\Definition;
 
 use PHPUnit\Framework\TestCase;
+use stdClass;
 use Yiisoft\Factory\Definition\ValueDefinition;
+use Yiisoft\Factory\Factory;
+use Yiisoft\Test\Support\Container\SimpleContainer;
 
 final class ValueDefinitionTest extends TestCase
 {
@@ -14,5 +17,25 @@ final class ValueDefinitionTest extends TestCase
         $definition = new ValueDefinition(42, 'integer');
 
         $this->assertSame('integer', $definition->getType());
+    }
+
+    public function testDoNotCloneObjectFromContainer(): void
+    {
+        $object = new stdClass();
+
+        $definition = new ValueDefinition($object, 'object');
+        $value = $definition->resolve(new SimpleContainer());
+
+        $this->assertSame($object, $value);
+    }
+
+    public function testCloneObjectFromFactory(): void
+    {
+        $object = new stdClass();
+
+        $definition = new ValueDefinition($object, 'object');
+        $value = $definition->resolve(new Factory());
+
+        $this->assertNotSame($object, $value);
     }
 }
