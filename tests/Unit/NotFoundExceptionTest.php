b3c82e6d3b833fc118b39b79fea1dbf0849191e7
Alexander Makarov <sam@rmcreative.ru>
2021-11-18 22:51:50 +0300

    Move NotFoundException from definitions (#148)

---
+++ tests/Unit/NotFoundExceptionTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Di\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use Yiisoft\Factory\NotFoundException;
+
+final class NotFoundExceptionTest extends TestCase
+{
+    public function testGetId(): void
+    {
+        $exception = new NotFoundException('test');
+
+        $this->assertSame('test', $exception->getId());
+    }
+
+    public function testMessage(): void
+    {
+        $exception = new NotFoundException('test');
+
+        $this->assertSame('No definition or class found or resolvable for test.', $exception->getMessage());
+    }
+}
